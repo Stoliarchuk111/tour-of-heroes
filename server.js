3f590ca7e21a7d173a472e940ab34ca51cbb3274
sTOLYARCHUK111 <75485066+Stoliarchuk111@users.noreply.github.com>
2021-04-22 09:20:09 +0300

    refactoring make it better

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // serve static files....
-app.use(express.static(`${__dirname}/dist/angular-heroku-deploy` ));
+app.use(express.static(`${__dirname}/dist\angular-tour-of-heroes` ));
 // send all requests to index.html
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/angular-heroku-deploy/index.html` ));
+res.sendFile(path.join(`${__dirname}/dist\angular-tour-of-heroes\index.html` ));
 });
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
